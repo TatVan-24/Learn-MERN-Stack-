2ea95fb2d4bc488d174817915758870fa9666ec7
Tatvan-24 <tatvannq@gmail.com>
2026-01-15 12:22:41 +0700

    150126 Add mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import  useState  from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from '@mui/material/Button'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Button from '@mui/material/Button';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
-import  pink  from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
+import { pink } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+import {
+  useColorScheme,
+} from '@mui/material/styles';
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
   return (
-    <>
-    <Typography variant="body2" color="text.secondary">test typography</Typography>
-
-     <Button variant="contained">Hello world</Button>
-     <Button variant="text">text</Button>
-     <Button variant="outlined">Out Lined</Button>
-    <br></br>
-     <AccessAlarmIcon />
-     <ThreeDRotation/>
-   <br></br>
-    <HomeIcon color="primary" />
-    <HomeIcon color="secondary" />
-
-    </>
-
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
   )
 }
 
-export default App
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ModeToggle />  
+      <br></br>
+      <Typography variant="body2" color="text.secondary">test typography</Typography>
+      <Button variant="contained">Hello world</Button>
+      <Button variant="text">text</Button>
+      <Button variant="outlined">Out Lined</Button>
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <br />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+    </>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import './index.css';
+import App from './App.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider  theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    </CssVarsProvider>
+  </StrictMode>
+);
